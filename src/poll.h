dcf4f6fc38bdf5092c45264c499d78387cf34b2a
Areg Nakashian <ss4crifice@gmail.com>
2026-02-01 19:36:31 -0500

    Refactor base64 function into parser.h

--- src/poll.h
+++ src/poll.h
@@ -26,26 +26,3 @@ private:
     httplib::SSLClient cli;
     httplib::Result res;
 };
-
-inline std::string base64(const std::string& in)
-{
-    static const char b64_table[] =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    std::string out;
-    int val = 0, valb = -6;
-    for (uint8_t c : in)
-    {
-        val = (val << 8) + c;
-        valb += 8;
-        while (valb >= 0)
-        {
-            out.push_back(b64_table[(val >> valb) & 0x3F]);
-            valb -= 6;
-        }
-    }
-    if (valb > -6)
-        out.push_back(b64_table[((val << 8) >> (valb + 8)) & 0x3F]);
-    while (out.size() % 4)
-        out.push_back('=');
-    return out;
-}
