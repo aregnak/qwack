4ec41e2cb23a0729932300d2e2fee0907849c17f
Areg Nakashian <ss4crifice@gmail.com>
2026-02-02 15:30:42 -0500

    not working

--- src/playerInfo.h
+++ src/playerInfo.h
@@ -13,24 +13,50 @@ struct PlayerInfo
     std::string rank;
 };
 
-// In-game info from /allgamedata
-struct StaticPLayer
-{
-    std::string riotID;
-    std::string team;
-    std::string role;
-};
+// // In-game info from /allgamedata
+// struct StaticPLayer
+// {
+//     std::string riotID;
+//     std::string team;
+//     std::string role;
+// };
 
-std::unordered_map<std::string, StaticPLayer> roleLookup;
+// std::unordered_map<std::string, StaticPLayer> roleLookup;
 
-void CacheRoles(const json& allGameData)
-{
-    roleLookup.clear();
+// void cacheRoles(const json& allGameData)
+// {
+//     roleLookup.clear();
 
-    for (const auto& p : allGameData["allPlayers"])
-    {
-        std::string riotId = NormalizeRiotID(p["riotIdGameName"], p["riotIdTagLine"]);
+//     for (const auto& p : allGameData["allPlayers"])
+//     {
+//         std::string riotId = NormalizeRiotID(p["riotIdGameName"], p["riotIdTagLine"]);
 
-        roleLookup[riotId] = { riotId, p["team"], p["position"] };
-    }
-}
+//         roleLookup[riotId] = { riotId, p["team"], p["position"] };
+//     }
+// }
+
+// static const std::vector<std::string> ROLE_ORDER = { "TOP", "JUNGLE", "MIDDLE", "BOTTOM",
+//                                                      "UTILITY" };
+
+// int roleIndex(const std::string& role)
+// {
+//     auto it = std::find(ROLE_ORDER.begin(), ROLE_ORDER.end(), role);
+//     return it == ROLE_ORDER.end() ? 99 : std::distance(ROLE_ORDER.begin(), it);
+// }
+
+// void sortPlayers(std::vector<PlayerInfo>& players)
+// {
+//     std::sort(players.begin(), players.end(),
+//               [&](const PlayerInfo& a, const PlayerInfo& b)
+//               {
+//                   const auto& A = roleLookup.at(a.riotID);
+//                   const auto& B = roleLookup.at(b.riotID);
+
+//                   // ORDER team first
+//                   if (A.team != B.team)
+//                       return A.team == "ORDER";
+
+//                   // Role order inside team
+//                   return RoleIndex(A.role) < RoleIndex(B.role);
+//               });
+// }
