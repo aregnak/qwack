e9736bcd885e62468f04c4fa18d5ac352f7c8578
Areg Nakashian <ss4crifice@gmail.com>
2026-02-26 12:26:53 -0500

    Add clarity comments in keyboard.h

--- src/keyboard.h
+++ src/keyboard.h
@@ -4,10 +4,12 @@
 
 inline bool IsTabDown()
 {
+    // VK_TAB is Tab.
     return (GetAsyncKeyState(VK_TAB) & 0x8000) != 0; //
 }
 
 inline bool killSwitch()
 {
+    // VK_NEXT is Page Down.
     return (GetAsyncKeyState(VK_NEXT) & 0x8000) != 0; //
 }
