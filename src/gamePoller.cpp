7634fc39940392f1acfe42606fb332334dfa70ce
Areg Nakashian <ss4crifice@gmail.com>
2026-03-06 23:46:59 -0500

    Cleanup conditional checks

--- src/gamePoller.cpp
+++ src/gamePoller.cpp
@@ -85,36 +85,33 @@ void GamePoller::handleLobbyState(LCUClient& lcuC, std::atomic<gameState>& gameS
 
     if (std::chrono::duration_cast<std::chrono::seconds>(_now - _lastPoll).count() > 5)
     {
-        if (_poller.update())
+        if (_poller.update() && !_playersLoaded)
         {
-            if (!_playersLoaded)
+            std::vector<PlayerInfo> newPlayers(10);
+            LCU_LOG("Polling Player Info...");
+
+            _poller.getSessionInfo(lcuC, newPlayers);
+
+            if (newPlayers.empty())
             {
-                std::vector<PlayerInfo> newPlayers(10);
-                LCU_LOG("Polling Player Info...");
-
-                _poller.getSessionInfo(lcuC, newPlayers);
-
-                if (newPlayers.empty())
-                {
-                    practicetool.store(true);
-                    QWACK_LOG("Gamemode is practice tool, skipping player info");
-                }
-
-                if (!practicetool.load())
-                {
-                    getAllPlayersInfo(newPlayers, lcuC);
-                }
-                _playersLoaded = true;
-
-                _players = std::move(newPlayers);
+                practicetool.store(true);
+                QWACK_LOG("Gamemode is practice tool, skipping player info");
             }
 
-            // Default gametime before actually loading in is 0.01810079999268055. Yeah idk either.
-            // This makes the INGAME state really mean loaded into the game, not just loading screen.
-            if (_poller.getGameTime() > 0.5f)
+            if (!practicetool.load())
             {
-                gameState.store(gameState::INGAME);
+                getAllPlayersInfo(newPlayers, lcuC);
             }
+            _playersLoaded = true;
+
+            _players = std::move(newPlayers);
+        }
+
+        // Default gametime before actually loading in is 0.01810079999268055. Yeah idk either.
+        // This makes the INGAME state really mean loaded into the game, not just loading screen.
+        if (_poller.getGameTime() > 0.5f)
+        {
+            gameState.store(gameState::INGAME);
         }
     }
 
