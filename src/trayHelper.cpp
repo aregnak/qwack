2f3e0a5e019cc067177f8e5d87c734bbf002c4d9
Areg Nakashian <ss4crifice@gmail.com>
2026-03-08 16:00:05 -0400

    Add show menu option in tray icon

--- src/trayHelper.cpp
+++ src/trayHelper.cpp
@@ -1,13 +1,18 @@
 #include "trayHelper.h"
 
+#include "log.h"
+#include "menuWindow.h"
+
 void callback_quit(void* userdata, SDL_TrayEntry* invoker)
 {
+    QWACK_LOG("Program terminated through tray icon.");
     SDL_Event e;
     e.type = SDL_EVENT_QUIT;
     SDL_PushEvent(&e);
 }
 
-TrayHelper::TrayHelper()
+TrayHelper::TrayHelper(MenuWindow& menu)
+    : _menu(menu)
 {
     tray = SDL_CreateTray(NULL, "My tray");
 
@@ -15,14 +20,24 @@ TrayHelper::TrayHelper()
     tmenu = SDL_CreateTrayMenu(tray);
 
     // Create a button in the context menu.
-    entry = SDL_InsertTrayEntryAt(tmenu, -1, "Quit", SDL_TRAYENTRY_BUTTON);
+    showEntry = SDL_InsertTrayEntryAt(tmenu, -1, "Open Menu", SDL_TRAYENTRY_BUTTON);
+    quitEntry = SDL_InsertTrayEntryAt(tmenu, -1, "Quit", SDL_TRAYENTRY_BUTTON);
 
     // Set the callback for the button
-    SDL_SetTrayEntryCallback(entry, callback_quit, NULL);
+    SDL_SetTrayEntryCallback(showEntry, callback_open, this);
+    SDL_SetTrayEntryCallback(quitEntry, callback_quit, NULL);
 }
 
 TrayHelper::~TrayHelper()
 {
     SDL_DestroyTray(tray);
     //
+}
+
+void TrayHelper::callback_open(void* userdata, SDL_TrayEntry* invoker)
+{
+    QWACK_LOG("HERE");
+
+    TrayHelper* self = static_cast<TrayHelper*>(userdata);
+    self->_menu.handleVisibility();
 }
--- src/trayHelper.h
+++ src/trayHelper.h
@@ -1,18 +1,24 @@
 #pragma once
 
 #include <SDL.h>
+#include "menuWindow.h"
 
 class TrayHelper
 {
 public:
-    TrayHelper();
+    TrayHelper(MenuWindow& menu);
     ~TrayHelper();
 
 private:
+    static void callback_open(void* userdata, SDL_TrayEntry* invoker);
+
     SDL_Tray* tray;
     SDL_TrayMenu* tmenu;
-    SDL_TrayEntry* entry;
+    SDL_TrayEntry* showEntry;
+    SDL_TrayEntry* quitEntry;
     SDL_Event e;
+
+    MenuWindow& _menu;
 
     // void callback_quit(void* userdata, SDL_TrayEntry* invoker);
 };
